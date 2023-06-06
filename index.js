7b15bce815c5980fc0135e185ae65c0666c514b3
Amotekun <97878695+Amotekun@users.noreply.github.com>
2023-06-05 19:46:35 -0700

    first commit

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ async function searchWeather(city) {
 
     if(response.status == 404) {
         document.querySelector(".error").style.display = "block";
-        document.querySelector(".weather").style.display = "none";
     } else {
         var data = await response.json();
 
@@ -52,7 +51,6 @@ async function searchWeather(city) {
             weatherIcon.src="./images/wind.png"
         };
 
-        document.querySelector(".weather").style.display = "block";
         document.querySelector(".error").style.display = "none";
 
     }
